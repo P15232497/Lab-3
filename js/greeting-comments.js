59a81d3b67d270fb24b44ab4c2af376c3a592830
Ravi Vaidya <P15232497@my365.dmu.ac.uk>
2017-11-09 14:29:26 +0000

    Initial commit

---
+++ js/greeting-comments.js
@@ -0,0 +1,54 @@
+(function(){
+/*
+  An "Immediately-Invoked Function Expression" (IIFE) contains
+  all the code. the final brackets () ensure that it executes.
+
+  This is good practice because:
+  - it keeps your variables out of the global namespace
+  - local references to objects decrease lookup times
+  - it is a foundation for further code organisation
+
+  It is also an "anonymous" function because it has/needs no name.
+*/
+  
+  /*
+    declare all the variables using 'let'. These get a reference
+    to the HTML elements we want by finding their ID
+  */
+  let myName = document.getElementById("my-name"); 
+  let userName = document.getElementById("user-name"); 
+  let getName = document.getElementById("get-name");
+  let usersname = document.getElementById("usersname");
+  
+  // changes the 'innerHTML' (text) of the element
+  function PerformGreeting() {
+    myName.innerHTML = userName.value;
+	usersname.innerHTML = userName.value;
+    event.preventDefault();
+	return false;
+  }
+  /*
+    Without setting the variables first, the above line would be really long:
+    document.getElementById("my-name").innerHTML = document.getElementById("user-name").value;
+  */
+
+  /*
+    Event Listeners keep your JavaScript out of the HTML -
+    professional developers avoid (e.g.) "onclick" in HTML tags.
+    This 'listens' for the user to submit the form.
+    You can attach multiple events to any single element.
+  */
+  if (typeof event === "undefined") {
+	  getName.onsubmit = PerformGreeting; //firefox
+  } else {
+	  getName.addEventListener("submit", PerformGreeting);
+  }
+
+  /*
+    Uncomment the line below - see what happens when you click
+    in the grey area outside the input "submit" button. Why?
+  */
+  
+  // getName.addEventListener("click", PerformGreeting);
+
+}());
